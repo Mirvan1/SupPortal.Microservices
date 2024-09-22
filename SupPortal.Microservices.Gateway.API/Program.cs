a9a02e946182a8f357c3a86c45d729e852482b7e
Mirvan1 <mirvan.sadiqli@gmail.com>
2024-09-22 16:25:56 +0300

    add services swagger's to gateway

--- SupPortal.Microservices.Gateway.API/Program.cs
+++ SupPortal.Microservices.Gateway.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using SupPortal.Microservices.Gateway.API;
@@ -22,9 +23,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 //builder.Services.AddEndpointsApiExplorer();
 //builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
+
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gateway API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "User API", Version = "v1" });
 
     c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
     {
@@ -35,6 +38,10 @@ builder.Services.AddSwaggerGen(c =>
     });
     c.OperationFilter<SecurityRequirementsOperationFilter>();
 });
+
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
@@ -50,8 +57,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 });
 builder.Services.AddAuthorization();
 
-builder.Services.AddOcelot(builder.Configuration);
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
 builder.Services.AddHealthChecksUI().AddInMemoryStorage();
 
 var app = builder.Build();
@@ -61,25 +67,30 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwaggerUI();
 }
 app.UseHealthChecksUI(opt =>
 {
     opt.UIPath = "/monitoring-ui";
     opt.UseRelativeApiPath = true;
 });
+ 
 app.UseMiddleware<ExceptionMiddleware>();
+app.UseSwaggerForOcelotUI(opt =>
+{
+
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+}).UseOcelot().Wait();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 
 app.UseAuthorization();
 
-app.UseSwaggerForOcelotUI(opt =>
-{
-    opt.PathToSwaggerGenerator = "/swagger/docs";
-}).UseOcelot().Wait();
+
 
 
 app.MapControllers();
 
 app.Run();
+
+ 
